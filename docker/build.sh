0e7c23d1c7d3a2c8189ff796e71a10e9d98ed332
Olivier Despesse <olived19@gmail.com>
2017-08-12 14:06:22 +0000

    Dockerize the service
    - Change the artifact's name
    - Use a lighter version of the OpenJdk container

--- docker/build.sh
+++ docker/build.sh
@@ -2,10 +2,10 @@
 
 VERSION=$1
 
-cp -p "../target/microGameOfLifeWebApi-${VERSION}.jar" .
+cp -p "../target/micro-gameoflife-webapi-${VERSION}.jar" .
 cp -p ../config.yml .
 sed -i "s/<VERSION>/${VERSION}/g" Dockerfile
 docker build -t "mgol-webapi:${VERSION}" .
 sed -i "s/${VERSION}/<VERSION>/g" Dockerfile
-rm "microGameOfLifeWebApi-${VERSION}.jar"
+rm "micro-gameoflife-webapi-${VERSION}.jar"
 rm config.yml
